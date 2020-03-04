8f08f61745ba8a0ea53ece665530e9f3781315ae
winnerdev2012 <winner.dev202@gmail.com>
2020-03-04 17:20:36 -0500

    Continue course

---
+++ 21-refreshing-array-functions/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  const numbers = [1,2,3];
+ 
+  const doubleArray = numbers.map(n => n * 2);
+
+  console.log('numbers: ', numbers)
+  console.log('doubleArray: ', doubleArray)
+});
+
