03c732ecefc0ad59f3ef33d25fd96582f942b21f
winnerdev2012 <winner.dev202@gmail.com>
2020-03-09 23:04:54 -0400

    Complete lecture 28

--- 27-create-react-app/react1/src/App.js
+++ 27-create-react-app/react1/src/App.js
@@ -6,13 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Task title='The dogs' />
+        <h1>Hi, I'm a React App</h1>
       </div>
     );
   }
--- 27-create-react-app/react1/src/Task.js
+++ 27-create-react-app/react1/src/Task.js
@@ -1,4 +1,4 @@
-import React,  { Compononet } from 'react';
+import React,  { Component } from 'react';
 
 class Task extends Component {
   render(){
