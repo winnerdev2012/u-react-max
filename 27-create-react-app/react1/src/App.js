88b3c895416b5fd72e60e958291735bca2acbf01
winnerdev2012 <winner.dev202@gmail.com>
2020-03-10 23:57:01 -0400

    Fix Task component

--- 27-create-react-app/react1/src/App.js
+++ 27-create-react-app/react1/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import './Task';
+import Task from './Task';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Task title="Shopping"/>
       </div>
     );
   }
