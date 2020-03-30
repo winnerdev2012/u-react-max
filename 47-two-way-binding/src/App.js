31ebc3a8d0543e23b15f2a0df6ac78f3befb857e
winnerdev2012 <winner.dev202@gmail.com>
2020-03-29 21:45:05 -0400

    Add 2 ways of styling

--- 47-two-way-binding/src/App.js
+++ 47-two-way-binding/src/App.js
@@ -13,7 +13,7 @@ const app = (props) => {
 
   const switchNameHandler = (newName) => {
     console.log('personsState: ', personsState)  
-    newName ? newName : newName = 'Bill';
+    // newName ? newName : newName = 'Bill';
     setPersonsState({
       persons: [
         {name: newName, age: 28, hometown: 'Munich'},
@@ -32,11 +32,20 @@ const app = (props) => {
       ]
     })
   }
-
+  
+  const style = {
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+  
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
-      <button onClick={switchNameHandler.bind(this, null)}>Switch Name</button>
+      <button 
+        onClick={switchNameHandler.bind(this, null)}
+        style={style}>Switch Name</button>
       <Person 
         name={personsState.persons[0].name} 
         age={personsState.persons[0].age}
