23a31b817ad432e7dd2c9187abdb7a70f739e0f9
winnerdev2012 <winner.dev202@gmail.com>
2020-03-29 20:22:56 -0400

    Add ptag background handler

--- 44-react-hooks/src/Person/Person.js
+++ 44-react-hooks/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <h3>My name is {props.name} and I am {props.age} years old.</h3>
+      <p onClick={props.onClick}>My name is {props.name} and I am {props.age} years old.</p>
       <p>My hometown is: {props.hometown}</p>
       {props.children}
     </div>
