50a3a201ce111fcb7cab66a8267319bcfb714359
winnerdev2012 <winner.dev202@gmail.com>
2020-04-04 19:48:01 -0400

    Add sass

--- 50-assignment/solution/src/UserOutput/UserOutput.js
+++ 50-assignment/solution/src/UserOutput/UserOutput.js
@@ -5,9 +5,11 @@ const UserOutput = (props) => {
   return (
     <div className="userOutput">
       <h3>Username: {props.username}</h3>
-      <button className="buttonstyle reverser" onClick={props.reverser}>Reverse UserName</button>
-      <button className="buttonstyle upcaser" onClick={props.upcaser}>Upcase UserName</button>
-      <button className="buttonstyle titlecaser" onClick={props.titlecaser}>TitleCaser UserName</button>
+      <div className="buttonrow">
+        <button className="buttonstyle reverser" onClick={props.reverser}>ReverseCase</button>
+        <button className="buttonstyle upcaser" onClick={props.upcaser}>UpCase</button>
+        <button className="buttonstyle titlecaser" onClick={props.titlecaser}>TitleCase</button>
+      </div>
       <p>Now is the time for all good men to come to the aid of their country.</p>
     </div>
   )
