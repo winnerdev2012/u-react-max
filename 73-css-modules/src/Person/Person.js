daef1bcd599f36745256f88bfbb22d15a566b669
winnerdev2012 <winner.dev202@gmail.com>
2020-08-02 10:03:04 -0400

    Add changes to Person.js, Person.css

--- 73-css-modules/src/Person/Person.js
+++ 73-css-modules/src/Person/Person.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import styled from 'styled-components'
-// import './Person.css';
-
-const StyledDiv = styled.div`
-    width: 65%;
-    margin: 16px auto;
-    border: 4px solid #222;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 18px;
-    text-align: center;
-
-    @media (min-width: 600px) {
-        border: 4px solid #e90f1d;
-    }
-    `
+import './Person.css';
 
 const person = (props) => {
     return (
-        <StyledDiv>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} defaultValue={props.name} />
             <input type="submit" onSubmit={props.submit}></input>
-        </StyledDiv>
+        </div>
     )
 };
 
